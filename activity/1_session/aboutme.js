665dcdc0db75bdc8ce4f7a1abd5d37b55c077774
ORJordiNadal <jordi.nadal@optimaretail.es>
2023-05-21 18:30:54 +0200

    4 Botones login incluidos en Actividad2

--- activity/1_session/aboutme.js
+++ activity/1_session/aboutme.js
@@ -6,4 +6,17 @@ const miPuestoActual = `CTO`;
 const fraseCompleta = `Hola, me llamo ${miNombre} y soy ${miProfesion}. 
 Estoy cursando este Máster porque me gustaría mejorar en mi puesto de ${miPuestoActual}`;
 
+// for (let i = 1; i <= 100; i++) {
+
+//     if (i % 7 === 0) {
+//       console.log(i);
+//     }
+//   }
+
+let i = 7;
+while (i < 100) {
+  console.log(i);
+  i += 7;
+}
+  
 console.log(fraseCompleta);
